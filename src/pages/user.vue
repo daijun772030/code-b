b7a6f24208952db2ad81e44ad819e9efe33e4857
daijun <904315004@qq.com>
2018-09-05 20:24:11 +0800

    代码提交

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="user">
-    <div class="banner">
-      <div class="banner-logo">
-        <img src="../../static/images/yaoqinghaoyou_lanzhudaojiatitou@2x.png" alt="">
-      </div>
-      <p>
-        {{userText}}
+    <div class="banner-logo">
+      <img src="../../static/images/yaoqinghaoyou_lanzhudaojiatitou@2x.png" alt="">
+    </div>
+    <p class="banner-p">
+      {{userText}}
+    </p>
+    <div class="banner-fg">
+      <img src="../../static/images/yaoqinghaoyou_zhuangshi@2x.png" alt="">
+    </div>
+    <div  class="user-content">
+      <img :src="yhImg.img1" alt="">
+      <p v-if="disablet">
+        懒猪到家链接：<a>http://www.pigcome.com</a>
       </p>
-      <div class="banner-fg">
-        <img src="../../static/images/yaoqinghaoyou_zhuangshi@2x.png" alt="">
-      </div>
-      <div  class="user-content">
-        <img :src="yhImg.img1" alt="">
-        <p v-if="disablet">
-          懒猪到家链接：<a>http://www.pigcome.com</a>
-        </p>
-        <cube-input
+      <!-- <cube-input
         v-if="disable"
         class="input"
         style="text-align:center"
@@ -25,19 +24,19 @@
         clearble
         placeholder="点击输入您的手机号"
         v-model="value"
-        ></cube-input>
-          <cube-button v-if="disable" @click="updata" class="button">点击领取</cube-button>
-      </div>
-      <div class="user-text">
-        <p>活动规则</p>
-        <ul>
-          <li>1.红包仅限懒猪到家新用户领取；</li>
-          <li>2.同一手机号、同一设备、同一支付账号视为同一用户；</li>
-          <li>3.新用户红包不与其他优惠叠加；</li>
-          <li>4.您邀请的好友需通过懒猪到家app或懒猪到家小程序下单，通过其他渠道下单不发放邀请奖励；</li>
-          <li>5.如有其它疑问请咨询懒猪到家客服。</li>
-        </ul>
-      </div>
+      ></cube-input> -->
+      <input class="input" type="text" v-if="input1" placeholder="点击输入您的手机号" v-model="value">
+        <cube-button v-if="disable" @click="updata" class="button">点击领取</cube-button>
+    </div>
+    <div class="user-text">
+      <p>活动规则</p>
+      <ul>
+        <li>1.红包仅限懒猪到家新用户领取；</li>
+        <li>2.同一手机号、同一设备、同一支付账号视为同一用户；</li>
+        <li>3.新用户红包不与其他优惠叠加；</li>
+        <li>4.您邀请的好友需通过懒猪到家app或懒猪到家小程序下单，通过其他渠道下单不发放邀请奖励；</li>
+        <li>5.如有其它疑问请咨询懒猪到家客服。</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -45,6 +44,9 @@
 export default {
   data () {
     return {
+      userId:null,
+      type:null,
+      input1:true,
       disable:true,
       disablet:false,
       userText:"88元新人红包等你来拿",
@@ -57,68 +59,127 @@ export default {
     }
   },
   created () {
-    
   },
   methods: {
+    GetRequest (url) {
+      var props = [];
+        if (url.indexOf("?") != -1) {    //判断是否有参数
+            var parameters = url.split("?")[1]; 
+            var parametersArr = parameters.split("&");
+            for(var i = 0; i < parametersArr.length; i++) {
+                props.push(parametersArr[i].split("="));
+            }
+        }
+        return props;
+    },
+    getUrl () {
+      debugger;
+      var url = location.href;
+      this.GetRequest(url)
+        var cs;
+        var request = this.GetRequest(url)
+        if(request != null && request.length == 1) {
+          cs = request[0][1]
+          console.log(cs);
+          this.userId = cs;
+        }
+    },
+
     updata () {
-      // var u = navigator.userAgent; 
-      // var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端 
-      // var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端 
-      // var webKit = u.indexOf('AppleWebKit') > -1; /*苹果、谷歌内核*/
-      //   var mobile = !!u.match(/AppleWebKit.*Mobile.*/); /*是否为移动终端*/
-      //   var weixin= u.toLowerCase().indexOf('micromessenger') > -1 /*是否是微信*/
+      this.getUrl();
+      var u = navigator.userAgent; 
+       var u = navigator.userAgent;
+      var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端 
+      var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端 
+      var webKit = u.indexOf('AppleWebKit') > -1; /*苹果、谷歌内核*/
+        var mobile = !!u.match(/AppleWebKit.*Mobile.*/); /*是否为移动终端*/
+        var weixin= u.toLowerCase().indexOf('micromessenger') > -1 /*是否是微信*/
       // alert('是否是Android：'+isAndroid);  
       // alert('是否是iOS：'+isiOS);
       //  alert('是否是：'+webKit);
       //  alert('是否是：'+mobile);
       // alert('是否是：'+weixin);
-      // this.$api('network',{params:{phone:this.value,type:'2'}}).then((res)=>{
-      //   console.log(res);
-      // })
-      // console.log(this.value)
-      this.$api('network',{params:{phone:this.value,type:'2'}}).then((res)=>{
-        // debugger;
-        var data = res.data;
-        console.log(data);
-        var text = data.split('(')[1]
-        var text1 = text.split('"')[1]
-        console.log(text1)
-        debugger;
-        if(text1 == '成功' ){
-          this.disable = false;
-          this.disablet = true;
-          this.yhImg.img1 = this.yhImg.img2
-        } else if (text1 == '该手机已被注册' ) {
-          this.yhImg.img1 = this.yhImg.img3
-          this.disable = false;
-          this.disablet = true;
-        }
-      })
+      console.log(this.value)
+      if (isAndroid) {
+        this.type = "0"
+        this.$api('network',{params:{phone:this.value,type:this.type,userid:this.userId}}).then((res)=>{
+          // debugger;
+          var data = res.data;
+          console.log(data);
+          var text = data.split('(')[1]
+          var text1 = text.split('"')[1]
+          console.log(text1)
+          // debugger;
+          if(text1 == '成功' ){
+            this.userText = '邀好友 得5...元红包';
+            this.disable = false;
+            this.input1 = false;
+            this.disablet = true;
+            this.yhImg.img1 = this.yhImg.img2
+          } else if (text1 == '该手机已被注册' ) {
+            this.input1 = false
+            this.yhImg.img1 = this.yhImg.img3;
+            this.userText = '邀好友 得5...元红包'
+            this.disable = false;
+            this.disablet = true;
+          }
+        })
+        console.log('这是安卓机')
+      }else if (isiOS) {
+        this.type = "1"
+        this.$api('network',{params:{phone:this.value,type:this.type,userid:this.userId}}).then((res)=>{
+          // debugger;
+          var data = res.data;
+          console.log(data);
+          var text = data.split('(')[1]
+          var text1 = text.split('"')[1]
+          console.log(text1)
+          // debugger;
+          if(text1 == '成功' ){
+            this.userText = '邀好友 得5...元红包';
+            this.disable = false;
+            this.input1 = false;
+            this.disablet = true;
+            this.yhImg.img1 = this.yhImg.img2
+          } else if (text1 == '该手机已被注册' ) {
+            this.input1 = false
+            this.yhImg.img1 = this.yhImg.img3;
+            this.userText = '邀好友 得5...元红包'
+            this.disable = false;
+            this.disablet = true;
+          }
+        })
+        console.log("这是苹果机")
+      }else if(webKit) {
+        console.log("这是pc")
+      }
+      
       console.log(this.value)
     }
   }
 }
 </script>
 <style scoped>
-  .user {
-    background-image: url('../../static/images/yanqinghaoyou_dabeijing@2x.png');
+  #app>.user {
+    background-image: url('../../static/images/yanqinghaoyou_dabeijing@3x.png');
     width: 100%;
     min-height: 100%;
-    min-height: 100%;
+    height: 100%;
+    /* height: 20px; */
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
   }
   .banner { 
     width: 100%;
-    height: 100%
+    min-height: 100%;
   }
   .banner-logo>img{
     width:197px;
     height:51px;
     background:rgba(0,212,160,1); 
   }
-  .banner>p{
+  .banner-p{
     text-align: center;
     height:50px;
     font-size:52px;
@@ -134,6 +195,7 @@ export default {
   .user-content {
     text-align: center;
     width: 594px;
+    height: 100%;
     margin: 0 auto;
     padding-top: 30px; 
   }
@@ -146,8 +208,6 @@ export default {
     height:65px;
     margin-top: 38px;
     text-align: center;
-  }
-  .user-content>input{
     vertical-align:middle;
     text-align: center;
     font-size:26px;
@@ -158,12 +218,16 @@ export default {
     width: 100%;
     height:65px;
     margin-top: 22px;
+    font-size:26px;
+    font-family:PingFang-SC-Medium;
+    font-weight:500;
+    color:rgba(255,255,255,1);
     background:rgba(0,203,153,1);
     border-radius:2px;
     text-align: center
   }
   .user-content>p{
-    margin: 51px 0 65px 0;
+    margin: 51px 0 30px 0;
     width: 100%;
     text-align:center;
     font-size:26px;
@@ -172,8 +236,11 @@ export default {
     text-decoration:underline;
     color:rgba(255,255,255,1);
   }
+  .user-text {
+    margin-bottom: 74px;
+  }
   .user-text>p{
-    padding: 51px 0 25px 0;
+    padding: 41px 0 25px 0;
     text-align: center;
     height:29px;
     font-size:26px;
