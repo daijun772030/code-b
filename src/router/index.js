b91ebb2ec80573e9b60ca9a754bf89ec9907f93d
daijun <904315004@qq.com>
2018-09-08 16:57:39 +0800

    代码提交星期六

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Home from '@/pages/home'
 import User from '@/pages/user'
-
+import All from '@/pages/all'
+import Law from '@/pages/law'
+import Privacy from '@/pages/privacy'
+import Agreement from '@/pages/agreement'
+import Rule from '@/pages/rule'
 Vue.use(Router)
 
 export default new Router({
@@ -17,6 +21,31 @@ export default new Router({
             path: '/home',
             name: 'home',
             component: Home
+        },
+        {
+            path: '/all',
+            name: 'All',
+            component: All
+        },
+        {
+            path: '/law',
+            name: 'law',
+            component: Law
+        },
+        {
+            path: '/privacy',
+            name: 'privacy',
+            component: Privacy
+        },
+        {
+            path: '/agreement',
+            name: 'agreement',
+            component: Agreement
+        },
+        {
+            path: '/rule',
+            name: 'rule',
+            component: Rule
         }
     ]
 })
